75d0178aa02418948e7a66fc1db95cb66663615a
Zachary Nguyen <zacktdn@gmail.com>
2023-01-27 21:29:18 +1000

    Initial Commit

---
+++ Spectre/SpectreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpectreApp.swift
+//  Spectre
+//
+//  Created by Zachary Nguyen on 27/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpectreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
